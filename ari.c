e1a2e5f24e71c10140b9f92f185968a01355ca21
yash0112 <85066851+yash0112@users.noreply.github.com>
2021-07-01 21:51:45 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -5,15 +5,14 @@
 #include <ctype.h>
 
 
-     string ari(string s){
+string ari(string s){
  int letters=0;
   for(int i=0; s[i]!='\0';i++)
   {
      if(isalpha(s[i])!=0)
         letters++;
      }
-
-     
+ 
  int words=0;
  for(int i=0; s[i]!='\0';i++){
   if(s[i]==' ')
@@ -35,39 +34,53 @@
      }
     
      
-float index=(4.71*((float)letters/words))+(0.5*((float)words/sentence))-21.43;
+   float index=(4.71*((float)letters/words))+(0.5*((float)words/sentence))-21.43;
    int ind=index/1;
-      ind++;
+   ind++;
+     
    if(ind<0 || ind==1)
       return "Kindergarden";
+     
    if(ind==2)
       return "First/Second Grade";
+     
    if(ind==3)
       return "Third Grade";
+     
    if(ind==4)
       return "Fourth Grade";
+     
    if(ind==5)
       return "Fifth Grade";
+     
    if(ind==6)
       return "Sixth grade";
+     
    if(ind==7)
       return "Seventh Grade";
+     
    if(ind==8)
       return "Eight Grade";
+     
    if(ind==9)
       return "Ninth Grade";
+     
    if(ind==10)
       return "Tenth Grade";
+     
    if(ind==11)
       return "Eleventh Grade";
+     
    if(ind==12)
       return "Twelfth Grade";
+     
    if(ind==13)
       return "College student";
+     
    if(ind==14 || ind>14)
       return "Professor";
    else
-     return "error";
+      return "error";
      
 
      }
