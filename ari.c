bc981c31a665d270e41480f70f561e65d885835e
yash0112 <85066851+yash0112@users.noreply.github.com>
2021-07-01 23:06:09 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -4,7 +4,6 @@
 #include <cs50.h>
 #include <ctype.h>
 
-
 string ari(string s){
  int letters=0;
   for(int i=0; s[i]!='\0';i++)
@@ -12,7 +11,8 @@ string ari(string s){
      if(isalpha(s[i])!=0)
         letters++;
      }
- 
+
+     
  int words=0;
  for(int i=0; s[i]!='\0';i++){
   if(s[i]==' ')
@@ -34,52 +34,57 @@ string ari(string s){
      }
     
      
-   float index=(4.71*((float)letters/words))+(0.5*((float)words/sentence))-21.43;
+ float index=(4.71*((float)letters/words))+(0.5*((float)words/sentence))-21.43;
    int ind=index/1;
-   ind++;
-     
-   if(ind<0 || ind==1)
+      ind++;
+ if(ind<0 || ind==1)
       return "Kindergarden";
+      else{
+          if(ind==2)
+             return "First/Second Grade";
+          else {
+             if(ind==3) 
+                 return "Third Grade";
+             else{
+               if(ind==4)
+                   return "Fourth Grade";
+               else{
+                   if(ind==5)
+                      return "Fifth Grade";
+                   else{
+                    if(ind==6)
+                       return "Sixth grade";
+                    else{
+                        if(ind==7)
+                           return "Seventh Grade";
+                        else{
+                              if(ind==8)
+                                 return "Eight Grade";
+                              else{
+                                if(ind==9)
+                                   return "Ninth Grade";
+                                else{
+                                   if(ind==10)
+                                     return "Tenth Grade";
+                                     else{
+                                       if(ind==11)
+                                           return "Eleventh Grade";
+                                       else{
+                                             if(ind==12)
+                                               return "Twelfth Grade";
+                                             else{
+                                                     if(ind==13)
+                                                       return "College student";
+                                                       else{
+                                                            if(ind==14 || ind>14)
+                                                                return "Professor";
+                                                                      else
+                                                                         return "error"; }}}}}}}}}}}}}
      
-   if(ind==2)
-      return "First/Second Grade";
+
+     }
      
-   if(ind==3)
-      return "Third Grade";
      
-   if(ind==4)
-      return "Fourth Grade";
-     
-   if(ind==5)
-      return "Fifth Grade";
-     
-   if(ind==6)
-      return "Sixth grade";
-     
-   if(ind==7)
-      return "Seventh Grade";
-     
-   if(ind==8)
-      return "Eight Grade";
-     
-   if(ind==9)
-      return "Ninth Grade";
-     
-   if(ind==10)
-      return "Tenth Grade";
-     
-   if(ind==11)
-      return "Eleventh Grade";
-     
-   if(ind==12)
-      return "Twelfth Grade";
-     
-   if(ind==13)
-      return "College student";
-     
-   if(ind==14 || ind>14)
-      return "Professor";
-   else
-      return "error";
-     
-}
+
+
+
