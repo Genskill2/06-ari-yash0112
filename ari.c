3b912eb56ba700727e1544b9302b9133f3b0b5fe
yash0112 <85066851+yash0112@users.noreply.github.com>
2021-07-02 10:49:44 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -26,9 +26,7 @@ string ari(string s){
   
   int sentence=0;
   for(int i=0; s[i]!='\0';i++){
-     if((s[i]=='.' ||s[i]=='?' || s[i]=='!') && s[i+1]==' ')
-     sentence++;
-     if(s[i]=='.' && s[i+1]=='\0')
+     if(s[i]=='.' ||s[i]=='?' || s[i]=='!')
      sentence++;
      }
     
